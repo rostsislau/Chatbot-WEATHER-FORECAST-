93310064f551b3f583c735997962796c9944167b
Rastsislau@git config --global user.name Rastsislaugit config --global user.name Rastsislau@ <rosts@mail.ru>
2025-04-01 12:23:12 +0300

    new commit

--- secret.py
+++ secret.py
@@ -1,10 +1,5 @@
 import telebot
 
-#bot = telebot.TeleBot('6913680192:AAFXyv8wiuFcZkg5R1AKLXurmU-vQjmPuHA')
-
-
-
 bot = telebot.TeleBot('7299698823:AAF-R1YM29xopr8pm63urEd8ACbeqvP0Pmo')
 API = '82f9ca8a481d778dd8030e5592633d8c'
 
-#api_2 = '7299698823:AAF-R1YM29xopr8pm63urEd8ACbeqvP0Pmo'
